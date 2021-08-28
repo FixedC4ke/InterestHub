94db31eb5884c6893fd761de6ade033289135e01
FixedC4ke <drmixe@gmail.com>
2021-08-28 11:57:33 +0500

    sorting out the backend

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import {Container, Form, Button, Card} from 'react-bootstrap';
+import { Container, Form, Button, Card, Alert } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 export default function RegisterPage(){
+    const [state, setState] = React.useState(false);
+    const [errorMsg, setErrorMsg] = React.useState('');
+    function submitHandler(e){
+        e.preventDefault();
+        let data = new FormData(e.target);
+
+        fetch('/register', {
+            method: 'post',
+            body: data
+        })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+                setState(data.success);
+                setErrorMsg(data.message);
+            });
+    }
+    if (state) { return <Redirect to='/'/> }
     return (
         <div>
             <Container>
                 <Card style={{margin: '20px 10%'}}>
                     <Card.Header as="h5">Регистрация</Card.Header>
                     <Card.Body>
-                        <Form method="POST" action="/register" encType="multipart/form-data">
+                        <Form onSubmit={submitHandler} encType="multipart/form-data">
+                            <Alert variant='danger' show={errorMsg.length>0}>{errorMsg}</Alert>
                             <Form.Group className="mb-3">
                                 <Form.Label>Имя пользователя</Form.Label>
-                                <Form.Control name="username" type="text" placeholder="Введите имя пользователя"/>
+                                <Form.Control required name="username" type="text" placeholder="Введите имя пользователя"/>
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Пароль</Form.Label>
@@ -33,7 +53,6 @@ export default function RegisterPage(){
                         </Form>
                     </Card.Body>
                 </Card>
-
             </Container>
         </div>
     );
--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { Sequelize, DataTypes } = require("sequelize");
-const session = require('express-session');
-const SeqeulizeStore = require('connect-session-sequelize')(session.Store);
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const multer = require('multer');
-const path = require('path');
-const flash = require('connect-flash');
-const bodyParser = require('body-parser');
+const session = require("express-session");
+const SeqeulizeStore = require("connect-session-sequelize")(session.Store);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const multer = require("multer");
+const path = require("path");
+const flash = require("connect-flash");
+const bodyParser = require("body-parser");
 let router = express.Router();
 
 const sequelize = new Sequelize(
@@ -33,181 +33,220 @@ const User = sequelize.define("user", {
     type: DataTypes.TEXT,
     allowNull: false,
   },
-  profilepictureid:{
+  profilepictureid: {
     type: DataTypes.TEXT,
-    defaultValue: 'defaultpfp.svg'
+    defaultValue: "defaultpfp.svg",
   },
   email: {
     type: DataTypes.TEXT,
-    allowNull: false
-  }
+    allowNull: false,
+  },
 });
 
-const Community = sequelize.define('community', {
+const Community = sequelize.define("community", {
   communityname: {
     type: DataTypes.TEXT,
-    allowNull: false 
+    allowNull: false,
   },
   description: {
-    type: DataTypes.TEXT
-  }
-})
+    type: DataTypes.TEXT,
+  },
+});
 
-passport.use(new LocalStrategy((username, password, done)=>{
-    User.findOne({where:{username: username}})
-        .then((user)=>{
-            if (!user) { return done(null, false, {
-              message: "Неверный логин и/или пароль"
-            }); }
-            bcrypt.compare(password, user.password)
-                .then(result=>{
-                    if (!result) { return done(null, false, {
-                      message: "Неверный логин и/или пароль"
-                    }); }
-                    else { return done(null, user); }
-                });
-        })
-        .catch(err=>done(err));
-}));
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ where: { username: username } })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, {
+            message: "Неверный логин и/или пароль",
+          });
+        }
+        bcrypt.compare(password, user.password).then((result) => {
+          if (!result) {
+            return done(null, false, {
+              message: "Неверный логин и/или пароль",
+            });
+          } else {
+            return done(null, user);
+          }
+        });
+      })
+      .catch((err) => done(err));
+  })
+);
 
-passport.serializeUser((user, done)=>done(null, user.id));
-passport.deserializeUser((id, done)=>{
-    User.findByPk(id)
-        .then(user=>done(null, user))
-        .catch(err=>done(err));
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => done(null, user))
+    .catch((err) => done(err));
 });
 
 let sessionStore = new SeqeulizeStore({
-    db: sequelize,
+  db: sequelize,
 });
 
-app.use(session({
-    secret: 'my secret',
+app.use(
+  session({
+    secret: "my secret",
     resave: false,
     saveUninitialized: true,
     store: sessionStore,
     cookie: {
-        maxAge: 1000*60*60*24
-    }
-}));
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-app.use('/', router);
+app.use("/", router);
 
-router.post('/login', multer().none(), function (req, res, next){
-  passport.authenticate('local', function(err, user, info){
-    if (err) { return next(err); }
-    if (!user) { return res.status(401).send({ message: info.message, success: false }); }
-    req.logIn(user, function(err){
-      if (err) { return next(err); }
-      return res.send({ message: '', success: true, user: user });
-    })
+router.post("/login", multer().none(), function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).send({ message: info.message, success: false });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.send({ message: "", success: true, user: user });
+    });
   })(req, res, next);
 });
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb)=>{
-    cb(null, 'uploads/')
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
   },
-  filename: (req, file, cb)=>{
-    cb(null, file.originalname)
-  }
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-router.post('/register', upload.single('avatar'), (req, res)=>{
-    let {username, password, confirmpass, email} = req.body;
-    if (password!=confirmpass){
-      res.sendStatus(400);
+router.post("/register", upload.single("avatar"), (req, res) => {
+  let { username, password, confirmpass, email } = req.body;
+  if (password != confirmpass) {
+    return res
+      .status(400)
+      .send({ message: "Подтверждение пароля не совпадает с введенным ранее" });
+  }
+  User.findOne({
+    where: {
+      username: username,
+    },
+  }).then((user) => {
+    if (user) {
+      return res.status(400).send({
+        message: "Пользователь с указанным никнеймом уже существует",
+        success: false
+      });
     }
-    bcrypt.genSalt(10, (err, salt)=>{
-        bcrypt.hash(password, salt)
-        .then((hash)=>{
-            if (!req.file) { req.file = { filename: 'defaultpfp.svg' }; }
-            let newuser = User.build(
-              {
-                username: username, 
-                password: hash,
-                profilepictureid: req.file.filename,
-                email: email
-              }
-            );
-            newuser.save()
-                .then(()=>{
-                  res.redirect('/login');
-                });
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt).then((hash) => {
+      if (!req.file) {
+        req.file = { filename: "defaultpfp.svg" };
+      }
+      let newuser = User.build({
+        username: username,
+        password: hash,
+        profilepictureid: req.file.filename,
+        email: email,
+      });
+      newuser.save()
+        .then(() => {
+          return res.status(200).send({ message: "", success: true });
+      })
+        .catch(err=>{
+          return res.status(403).send({ message: err, success: false});
         });
     });
+  });
 });
 
-router.get('/logout', (req, res)=>{
-    req.logout();
-    res.send(200);
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send(200);
 });
 
-router.get('/profilepicture/:userid?', (req, res, next)=>{
-  if (!req.params.userid) { next(); return; }
-  User.findByPk(req.params.userid)
-    .then((user)=>{
-      if (!user) { res.send({message: 'Пользователя с таким ID не существует'}); }
-      res.sendFile(path.join(__dirname, 'uploads', user.profilepictureid));
-    });
-})
+router.get("/profilepicture/:userid?", (req, res, next) => {
+  if (!req.params.userid) {
+    next();
+    return;
+  }
+  User.findByPk(req.params.userid).then((user) => {
+    if (!user) {
+      res.send({ message: "Пользователя с таким ID не существует" });
+    }
+    res.sendFile(path.join(__dirname, "uploads", user.profilepictureid));
+  });
+});
 
-router.get('/profilepicture', (req, res)=>{
-  if (!req.isAuthenticated()) { res.sendStatus(403); }
-  res.sendFile(path.join(__dirname, 'uploads', req.user.profilepictureid));
-})
+router.get("/profilepicture", (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.sendStatus(401);
+  }
+  res.sendFile(path.join(__dirname, "uploads", req.user.profilepictureid));
+});
 
-router.get('/getcurrentuser', (req, res)=>{
-  if (req.user){
+router.get("/getcurrentuser", (req, res) => {
+  if (req.user) {
     res.send({
       isAuthenticated: true,
       id: req.user.id,
       username: req.user.username,
-      profilepictureid: req.user.profilepictureid
+      profilepictureid: req.user.profilepictureid,
     });
+  } else {
+    res.send({ isAuthenticated: false });
   }
-  else { res.send({ isAuthenticated: false });}
 });
 
-router.get('/getuser/:id', (req, res)=>{
-  if (req.isAuthenticated()){
-    User.findByPk(req.params.id)
-      .then(user=>{
-        if (user){
-          res.send({
-            username: user.username,
-            profilepictureid: user.profilepictureid
-          });
-        }
-        else { res.send({message: 'Пользователя с таким ID не существует'}); }
-      });
+router.get("/getuser/:id", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.findByPk(req.params.id).then((user) => {
+      if (user) {
+        res.send({
+          username: user.username,
+          profilepictureid: user.profilepictureid,
+        });
+      } else {
+        res.send({ message: "Пользователя с таким ID не существует" });
+      }
+    });
+  } else {
+    res.sendStatus(401);
   }
-  else { res.sendStatus(403); }
-})
-
-router.get('/communities', (req, res)=>{
-  Community.findAll()
-    .then(result => res.send(result));
 });
 
-router.post('/newcommunity', (req, res)=>{
-  let {communityname, description} = req.body;
-  let newcommunity = Community.build({communityname: communityname, description: description});
-  newcommunity.save()
-    .then(()=>res.redirect('/communities'));
-})
+router.get("/communities", (req, res) => {
+  Community.findAll().then((result) => res.send(result));
+});
 
-sequelize.sync({force: true}).then(() => {
-  console.log('Model sync');
-  sessionStore.sync().then(()=>{
-    console.log('Storage sync');
+router.post("/newcommunity", (req, res) => {
+  let { communityname, description } = req.body;
+  let newcommunity = Community.build({
+    communityname: communityname,
+    description: description,
+  });
+  newcommunity.save().then(() => res.redirect("/communities"));
+});
+
+sequelize.sync({ force: true }).then(() => {
+  console.log("Model sync");
+  sessionStore.sync().then(() => {
+    console.log("Storage sync");
     app.listen(PORT).then = () => {
       console.log(`Сервер запущен на порту ${PORT}`);
     };
